a7a99567f4ede77df4c1f6349abb6dd1847f6240
bilal060 <ahmad_4al@hotmail.com>
2023-12-14 13:18:51 +0500

    added moving border aniamtion

--- src/app/components/HomePage/HeroSection.js
+++ src/app/components/HomePage/HeroSection.js
@@ -30,7 +30,7 @@ const HeroSection = ({ createRef, activeSection }) => {
         <Image
           src={VerticalDots}
           alt="HorizentalDots"
-          className="hidden md:block 2xl:my-4 mb-4"
+          className="hidden 2xl:block 2xl:my-4 mb-4"
         />
         <h1 className="reveal-text lg:w-[819px] text-56px gradient-text-green">
           Unleash Your Business Potential With Our Custom IT Solutions
@@ -48,16 +48,36 @@ const HeroSection = ({ createRef, activeSection }) => {
           />
         </div>
       </div>
-      <div className="2xl:container 2xl:mx-auto reveal-text">
-        <div className="home-rectangle-bg bg-cover bg-center bg-no-repeat  flex flex-col justify-center w-full sm:w-[70%] h-[160px] sm:h-[200px] 2xl:h-[280px] lg:w-[618px]">
+      <div className="md:container md:mx-auto reveal-text">
+        <div className="react-angle mt-[40px] 2xl:mt-4 flex relative sm:w-[70%] sm:ml-[40px] md:ml-0  h-[130px] sm:h-[140px] md:h-[160px] 2xl:h-[170px] lg:w-[730px]">
+          <div className="h-[1px] absolute top-0 text-[#393939] bg-[#393939] w-full rounded">
+            <div className="moving-border-animation">
+              <span></span>
+            </div>
+          </div>
+          <div className=" absolute left-[-75px] sm:left-[-50px] lg:left-0 text-[#393939] bg-[#393939] rounded  border-t border-[#393939] rotate-90 w-[180px] md:w-[220px] top-[65px] md:top-[80px] 2xl:w-[250px] 2xl:top-[85px] 2xl:left-[-50px]">
+            <div className="moving-border-animation">
+              <span></span>
+            </div>
+          </div>
+          <div className=" text-[#393939] absolute right-[-75px] sm:right-[-50px] lg:right-0 bg-[#393939] rounded border-t border-[#393939] rotate-90 w-[180px] md:w-[220px] top-[65px] md:top-[80px] 2xl:w-[250px] 2xl:top-[85px] 2xl:right-[-50px]">
+            <div className="moving-border-animation">
+              <span></span>
+            </div>
+          </div>
+          <div className="h-[1px] text-[#393939] absolute bottom-0 bg-[#393939] w-full rounded">
+            <div className="moving-border-animation">
+              <span></span>
+            </div>
+          </div>
           <div className="flex w-[70%] items-center justify-around mx-auto">
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center h-full justify-center border-r border-[#393939] w-full">
               <h3 className="text-56px gradient-text-green">450+</h3>
               <p className="text-18px text-lightGray text-[14px] ">
                 Project Done
               </p>
             </div>
-            <div className="flex flex-col items-center">
+            <div className="flex flex-col items-center w-full">
               <h3 className=" text-56px  gradient-text-green">200+</h3>
               <p className=" text-18px text-lightGray text-[14px]">
                 Happy Clients
